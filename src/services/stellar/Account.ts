4e41154a9acdbfcb62bdd7de1fbf7ae93afd2eed
Joaquin Akerman <55959401+JoaquinAkerman@users.noreply.github.com>
2023-12-13 18:13:34 -0300

    Feature/Stellar Sdk Implementation  (#3)

---
+++ src/services/stellar/Account.ts
@@ -0,0 +1,24 @@
+import { Keypair } from 'stellar-sdk';
+import { server } from './utils';
+
+class Account {
+  public publicKey: string;
+  public secretKey: string;
+
+  constructor(keypair: Keypair) {
+    this.publicKey = keypair.publicKey();
+    this.secretKey = keypair.secret();
+  }
+
+  async fundWithFriendBot(): Promise<Account> {
+    await server.friendbot(this.publicKey).call();
+    return this;
+  }
+
+  static async create(): Promise<Account> {
+    const keypair = Keypair.random();
+    return new Account(keypair);
+  }
+}
+
+export { Account };
