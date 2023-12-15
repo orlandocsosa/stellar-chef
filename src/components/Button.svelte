a734fdc6f29bb77c2acd8bcbc9fc395f3548d4c2
Joaquin Akerman <55959401+JoaquinAkerman@users.noreply.github.com>
2023-12-15 14:27:24 -0300

    Add id prop to Button component

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -1,6 +1,7 @@
 <script>
   export let label = '';
   export let onClick = () => {};
+  export let id = '';
 
   function handleClick() {
     onClick();
@@ -8,6 +9,7 @@
 </script>
 
 <button
+  {id}
   class="bg-white hover:bg-gray-200 text-black py-2 px-4 border border-black w-full mx-2"
   on:click={handleClick}
   style="box-shadow: 3px 3px 0px 0px black;"
