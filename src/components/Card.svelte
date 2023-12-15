c6fc268180dc0c38dd4f01abdcfe1b960f48d3a9
Joaquin Akerman <55959401+JoaquinAkerman@users.noreply.github.com>
2023-12-15 10:54:49 -0300

    Add Card component

---
+++ src/components/Card.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  export let title: string;
+  export let content: string = '';
+</script>
+
+<div class="border-4 border-black p-5 w-72 mx-2 bg-white shadow-lg mt-4">
+  <h1 class="text-xl font-bold mb-5">{title}</h1>
+  <p>{content}</p>
+  <slot />
+</div>
