6d5d785dbb0da17a1003259c6a522766039f23e1
Joaquin Akerman <55959401+JoaquinAkerman@users.noreply.github.com>
2023-12-15 10:54:28 -0300

    Add Button component

---
+++ src/components/Button.svelte
@@ -0,0 +1,17 @@
+<script>
+  export let label = '';
+  export let onClick = () => {};
+
+  function handleClick() {
+    console.log('Button clicked!');
+    onClick();
+  }
+</script>
+
+<button
+  class="bg-white hover:bg-gray-200 text-black py-2 px-4 border border-black w-full mx-2"
+  on:click={handleClick}
+  style="box-shadow: 3px 3px 0px 0px black;"
+>
+  {label}
+</button>
