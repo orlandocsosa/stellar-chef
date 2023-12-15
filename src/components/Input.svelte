c6458fed16aae3852dd367f97d62350a8f1b1bfd
Joaquin Akerman <55959401+JoaquinAkerman@users.noreply.github.com>
2023-12-15 10:55:05 -0300

    Add Input component

---
+++ src/components/Input.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  export let id: string;
+  export let value: string;
+  export let label: string;
+  export let readonly: boolean = false;
+</script>
+
+<div>
+  <label for={id} class="block mb-2">{label}:</label>
+  <input {id} type="text" bind:value class="w-full p-2 border-4 border-black mb-4" {readonly} />
+</div>
