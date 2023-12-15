9dea7cf396945714fa6bdf3bc0a63e9505e04563
Joaquin Akerman <55959401+JoaquinAkerman@users.noreply.github.com>
2023-12-15 14:27:40 -0300

    Add Checkbox component

---
+++ src/components/Checkbox.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  export let label = '';
+  export let checked = false;
+  export let id = '';
+</script>
+
+<label class="block mb-2">
+  <input {id} type="checkbox" bind:checked />
+  {label}
+</label>
