e09d9f85b01874cae9522fb4abe991512d318cab
Joaquin Akerman <55959401+JoaquinAkerman@users.noreply.github.com>
2023-12-15 14:27:50 -0300

    Add NumberInput component

---
+++ src/components/NumberInput.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  export let label = '';
+  export let value = 0;
+  export let id = '';
+</script>
+
+<label class="block mb-2" for={id}>{label}</label>
+<input {id} type="number" bind:value class="border-2 border-black rounded mb-2" />
