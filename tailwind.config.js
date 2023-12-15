18b54a53e53d9bfa7fd8d5ab392fa3b2eda91345
Joaquin Akerman <55959401+JoaquinAkerman@users.noreply.github.com>
2023-12-15 10:52:38 -0300

    Add tailwind.config.js file

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ['./src/**/*.svelte', './src/**/*.html'],
+  darkMode: false,
+  theme: {
+    extend: {}
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+};
