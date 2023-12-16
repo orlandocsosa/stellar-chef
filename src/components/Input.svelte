52d420422a0a2f73d58b7fe03ed84aa178e9ec1c
Joaquin Akerman <55959401+JoaquinAkerman@users.noreply.github.com>
2023-12-16 13:47:34 -0300

    Refactor Input component to handle different input types

--- src/components/Input.svelte
+++ src/components/Input.svelte
@@ -1,11 +1,37 @@
 <script lang="ts">
+  export let value: string | number;
   export let id: string;
-  export let value: string;
-  export let label: string;
+  export let disabled: boolean = false;
   export let readonly: boolean = false;
+  export let type: string = 'text';
+
+  function handleInput(event: Event & { currentTarget: HTMLInputElement }) {
+    if (type === 'number') {
+      value = parseFloat(event.currentTarget.value);
+    } else {
+      value = event.currentTarget.value.toString().replace(/\s/g, '');
+    }
+  }
 </script>
 
-<div>
-  <label for={id} class="block mb-2">{label}:</label>
-  <input {id} type="text" bind:value class="w-full p-2 border-4 border-black mb-4" {readonly} />
-</div>
+{#if type === 'text'}
+  <input
+    {id}
+    bind:value
+    on:input={handleInput}
+    type="text"
+    {disabled}
+    {readonly}
+    class="w-full p-2 border-4 border-black mb-4"
+  />
+{:else if type === 'number'}
+  <input
+    {id}
+    bind:value
+    on:input={handleInput}
+    type="number"
+    {disabled}
+    {readonly}
+    class="border-2 border-black rounded mb-2"
+  />
+{/if}
