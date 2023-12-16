fa19a759ad979a858039cfde65751b20591b48de
Joaquin Akerman <55959401+JoaquinAkerman@users.noreply.github.com>
2023-12-16 16:16:55 -0300

    Add hasToAcceptSpaces and maxlength props to Input component

--- src/components/Input.svelte
+++ src/components/Input.svelte
@@ -4,34 +4,24 @@
   export let disabled: boolean = false;
   export let readonly: boolean = false;
   export let type: string = 'text';
+  export let hasToAcceptSpaces: boolean = false;
+  export let maxlength: number = 50;
 
-  function handleInput(event: Event & { currentTarget: HTMLInputElement }) {
-    if (type === 'number') {
-      value = parseFloat(event.currentTarget.value);
-    } else {
-      value = event.currentTarget.value.toString().replace(/\s/g, '');
+  function handleSpaces(event: Event & { currentTarget: HTMLInputElement }) {
+    value = event.currentTarget.value;
+    if (!hasToAcceptSpaces) {
+      value = value.toString().replace(/\s/g, '');
     }
   }
 </script>
 
-{#if type === 'text'}
-  <input
-    {id}
-    bind:value
-    on:input={handleInput}
-    type="text"
-    {disabled}
-    {readonly}
-    class="w-full p-2 border-4 border-black mb-4"
-  />
-{:else if type === 'number'}
-  <input
-    {id}
-    bind:value
-    on:input={handleInput}
-    type="number"
-    {disabled}
-    {readonly}
-    class="border-2 border-black rounded mb-2"
-  />
-{/if}
+<input
+  {id}
+  {value}
+  on:input={handleSpaces}
+  {type}
+  {disabled}
+  {readonly}
+  {maxlength}
+  class={type === 'text' ? 'mb-4 border-4 w-full p-2 border-black' : 'mb-2 border-2 rounded'}
+/>
