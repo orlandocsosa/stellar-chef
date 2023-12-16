09e4ffc4a61ceb4e8b15281a72dd494a3b64471d
Joaquin Akerman <55959401+JoaquinAkerman@users.noreply.github.com>
2023-12-16 13:47:20 -0300

    Delete NumberInput component

--- src/components/NumberInput.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-  export let label = '';
-  export let value = 0;
-  export let id = '';
-</script>
-
-<label class="block mb-2" for={id}>{label}</label>
-<input {id} type="number" bind:value class="border-2 border-black rounded mb-2" />
