ad802c7ffdc1a869999275752cbe8bb98be9b435
Joaquin Akerman <55959401+JoaquinAkerman@users.noreply.github.com>
2023-12-17 15:40:18 -0300

    Add disabled prop to Button component

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -2,6 +2,7 @@
   export let label = '';
   export let onClick = () => {};
   export let id = '';
+  export let disabled = false;
 
   function handleClick() {
     onClick();
@@ -10,6 +11,7 @@
 
 <button
   {id}
+  {disabled}
   class="bg-white hover:bg-gray-200 text-black py-2 px-4 border border-black w-full mx-2"
   on:click={handleClick}
   style="box-shadow: 3px 3px 0px 0px black;"
