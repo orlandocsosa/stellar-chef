93e6644ee3a47a03fe4bf9b278d661d24e7b9da3
Joaquin Akerman <55959401+JoaquinAkerman@users.noreply.github.com>
2023-12-18 13:57:21 -0300

    Update button box-shadow style

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -14,7 +14,7 @@
   {disabled}
   class="bg-white hover:bg-gray-200 text-black py-2 px-4 border border-black w-full mx-2"
   on:click={handleClick}
-  style="box-shadow: 3px 3px 0px 0px black;"
+  style="box-shadow: 5px 5px 0px 0px black;"
 >
   {label}
 </button>
