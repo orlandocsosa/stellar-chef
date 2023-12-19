0bdb6768f33ff3323f6d640d762b465589edb11e
Joaquin Akerman <55959401+JoaquinAkerman@users.noreply.github.com>
2023-12-19 13:14:31 -0300

    Create CopyButton
    This commit introduces a new Svelte component with the ability to copy text to the clipboard.

---
+++ src/components/CopyButton.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  export let textToCopy: string | number;
+
+  function copyToClipboard() {
+    navigator.clipboard.writeText(textToCopy.toString());
+  }
+</script>
+
+<button
+  on:click={copyToClipboard}
+  class="ml-2 p-1 text-xs rounded text-black hover:bg-gray-200 transform translate-y-[-2px]">Copy</button
+>
