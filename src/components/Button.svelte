09e8f8332d551663f51fc65bb3b5dab5643ecd6c
Joaquin Akerman <55959401+JoaquinAkerman@users.noreply.github.com>
2023-12-19 10:39:36 -0300

    Refactor Button component's click event handling

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -3,17 +3,13 @@
   export let onClick = () => {};
   export let id = '';
   export let disabled = false;
-
-  function handleClick() {
-    onClick();
-  }
 </script>
 
 <button
   {id}
   {disabled}
   class="bg-white hover:bg-gray-200 text-black py-2 px-4 border border-black w-full mx-2"
-  on:click={handleClick}
+  on:click={onClick}
   style="box-shadow: 5px 5px 0px 0px black;"
 >
   {label}
