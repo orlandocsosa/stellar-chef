53c339e7d927a20b2a1173be804cc3dce352551b
Joaquin Akerman <55959401+JoaquinAkerman@users.noreply.github.com>
2023-12-19 13:12:55 -0300

    Add AssetOutput component
    Adds a new dynamic display component that accepts a value, id, and type as props. The component leverages the CopyButton Svelte component to enable easy copying of the displayed content.

---
+++ src/components/AssetOutput.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import CopyButton from './CopyButton.svelte';
+  export let value: string | number;
+  export let id: string;
+  export let type: string = 'text';
+</script>
+
+<CopyButton textToCopy={value} />
+<div
+  {id}
+  contenteditable="false"
+  class={type === 'text'
+    ? 'mb-4 w-full p-2 font-bold overflow-hidden whitespace-nowrap overflow-ellipsis'
+    : 'mb-2 font-bold overflow-hidden whitespace-nowrap overflow-ellipsis'}
+>
+  {value}
+</div>
