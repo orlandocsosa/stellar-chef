ea070240d83568bead17913d59f515c121dc8cb6
Joaquin Akerman <55959401+JoaquinAkerman@users.noreply.github.com>
2023-12-21 14:04:32 -0300

    Refactor error handling in submitTransaction function

--- src/services/stellar/utils.ts
+++ src/services/stellar/utils.ts
@@ -28,15 +28,19 @@ async function submitTransaction(transaction: Transaction): Promise<string> {
     await server.submitTransaction(transaction);
     return 'Transaction submitted successfully';
   } catch (e: unknown) {
-    let stellarErrorCode;
-    if (e instanceof Error) {
-      const errorResponse = e as any;
-      stellarErrorCode = errorResponse.response?.data?.extras?.result_codes;
+    if (!(e instanceof Error)) {
+      throw new Error('An error occurred while submitting the transaction');
     }
-    if (stellarErrorCode !== undefined) {
-      throw new Error(JSON.stringify(stellarErrorCode));
+
+    const errorResponse = e as any;
+    const stellarErrorCode = errorResponse.response?.data?.extras?.result_codes;
+
+    if (stellarErrorCode === undefined) {
+      throw new Error('An error occurred while submitting the transaction');
     }
-    throw new Error('An error occurred while submitting the transaction');
+
+    throw new Error(JSON.stringify(stellarErrorCode));
   }
 }
+
 export { buildTransaction, server, submitTransaction };
