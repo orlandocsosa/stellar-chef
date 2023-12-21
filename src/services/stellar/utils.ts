b01abd7e60bd0c5b0d57475e83e6b77dbbc817a6
Joaquin Akerman <55959401+JoaquinAkerman@users.noreply.github.com>
2023-12-21 13:46:07 -0300

    Refactor import statements and formatting in stellar/utils.ts

--- src/services/stellar/utils.ts
+++ src/services/stellar/utils.ts
@@ -1,22 +1,11 @@
-import {
-  type Account,
-  TransactionBuilder,
-  BASE_FEE,
-  type xdr,
-  type Transaction,
-  Horizon
-} from 'stellar-sdk';
+import { type Account, TransactionBuilder, BASE_FEE, type xdr, type Transaction, Horizon } from 'stellar-sdk';
 
 const STELLAR_NETWORK_URL = import.meta.env.VITE_STELLAR_NETWORK_URL;
-const STELLAR_NETWORK_PASSPHRASE = import.meta.env
-  .VITE_STELLAR_NETWORK_PASSPHRASE;
+const STELLAR_NETWORK_PASSPHRASE = import.meta.env.VITE_STELLAR_NETWORK_PASSPHRASE;
 
 const server = new Horizon.Server(STELLAR_NETWORK_URL);
 
-function buildTransaction(
-  sourceAccount: Account,
-  operations: xdr.Operation[]
-): Transaction {
+function buildTransaction(sourceAccount: Account, operations: xdr.Operation[]): Transaction {
   const transaction = new TransactionBuilder(sourceAccount, {
     fee: BASE_FEE,
     networkPassphrase: STELLAR_NETWORK_PASSPHRASE
