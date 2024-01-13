30448e2950183411548ea975c9b73082139664cd
Joaquin Akerman <55959401+JoaquinAkerman@users.noreply.github.com>
2024-01-13 20:09:51 -0300

    Wrap Inputs in Form and Add Required Attribute
    In this commit, we have wrapped the Input components inside a form element. This allows us to use the 'required' attribute on the inputs, which prevents the form from being submitted when these inputs are empty.

--- src/routes/recipe/clawback/+page.svelte
+++ src/routes/recipe/clawback/+page.svelte
@@ -69,42 +69,45 @@
 </script>
 
 <div class="flex justify-center">
-  <Card title="Clawback Asset ">
-    <label for="asset-code">
-      Asset Code
-      <Input id="asset-code" bind:value={assetCode} disabled={isLoading} />
-    </label>
-    <label for="issuer-secret-key">
-      Issuer Secret Key
-      <Input id="issuer-secret-key" bind:value={issuerSecretKey} disabled={isLoading} />
-    </label>
-    <label for="clawback-account">
-      Clawback Account
-      <Input id="clawback-account" bind:value={clawbackAccount} disabled={isLoading} />
-    </label>
-    <label for="amount">
-      Amount
-      <Input id="amount" bind:value={amountForClawback} type="number" disabled={isLoading || isClawbackAllEnabled} />
-    </label>
-    <label for="is-clawback-all-enabled">
-      Clawback All
-      <input type="checkbox" id="is-clawback-all-enabled" bind:checked={isClawbackAllEnabled} disabled={isLoading} />
-    </label>
+  <form on:submit|preventDefault={performClawback}>
+    <Card title="Clawback Asset ">
+      <label for="asset-code">
+        Asset Code
+        <Input id="asset-code" bind:value={assetCode} disabled={isLoading} required />
+      </label>
+      <label for="issuer-secret-key">
+        Issuer Secret Key
+        <Input id="issuer-secret-key" bind:value={issuerSecretKey} disabled={isLoading} required />
+      </label>
+      <label for="clawback-account">
+        Clawback Account
+        <Input id="clawback-account" bind:value={clawbackAccount} disabled={isLoading} required />
+      </label>
+      <label for="amount">
+        Amount
+        <Input
+          id="amount"
+          bind:value={amountForClawback}
+          type="number"
+          disabled={isLoading || isClawbackAllEnabled}
+          required
+        />
+      </label>
+      <label for="is-clawback-all-enabled">
+        Clawback All
+        <input type="checkbox" id="is-clawback-all-enabled" bind:checked={isClawbackAllEnabled} disabled={isLoading} />
+      </label>
 
-    <div class="flex justify-center mt-5">
-      <Button
-        id="clawback-button"
-        label={isLoading ? 'Performing...' : 'Perform Clawback'}
-        onClick={performClawback}
-        disabled={isLoading}
-      />
-    </div>
-    <div id="status" class="h-auto max-h-12 overflow-y-auto overflow-x-hidden mt-4">
-      {#if isTransactionSuccessful}
-        Transaction successful <TransactionInfo {transactionHash} />
-      {:else}
-        {@html status}
-      {/if}
-    </div>
-  </Card>
+      <div class="flex justify-center mt-5">
+        <Button id="clawback-button" label={isLoading ? 'Performing...' : 'Perform Clawback'} disabled={isLoading} />
+      </div>
+      <div id="status" class="h-auto max-h-12 overflow-y-auto overflow-x-hidden mt-4">
+        {#if isTransactionSuccessful}
+          Transaction successful <TransactionInfo {transactionHash} />
+        {:else}
+          {@html status}
+        {/if}
+      </div>
+    </Card>
+  </form>
 </div>
