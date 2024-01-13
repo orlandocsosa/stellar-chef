24c3052c8120dae4049e1840a45279093ca81af2
Joaquin Akerman <55959401+JoaquinAkerman@users.noreply.github.com>
2024-01-12 21:55:12 -0300

    Add Cypress test for Clawback Asset Page
    Add Cypress test that simulates the clawback operation for an account on the Clawback Asset Page. The test checks the functionality of the clawback operation, including the validation of input fields and the display of success and error messages.
    Network requests to Horizon Testnet are mocked using Cypress fixtures. Which also are included on this commit

--- cypress/e2e/clawbackTests.cy.ts
+++ cypress/e2e/clawbackTests.cy.ts
@@ -1,24 +1,54 @@
-describe('Clawback Asset Page', () => {
+describe('Test Asset Page', () => {
   beforeEach(() => {
+    cy.intercept(
+      'GET',
+      'https://horizon-testnet.stellar.org/accounts/GANFMU7SHOFXUM6OF32BGKADD6QSEEP25E2ZVJ6PTJUKI4WORZY6Y6SI',
+      { fixture: 'issuerAccount.json' }
+    ).as('load Issuer Account request');
+
+    cy.intercept('POST', '**/transactions', { fixture: 'transaction.json' }).as('performClawback');
+    cy.intercept(
+      'GET',
+      'https://horizon-testnet.stellar.org/accounts/GDAM4TVOXH5RA36KIQLG6JZBM5LB2LMWTTHHJMXCZIPVORNHV4AFPQW3',
+      { fixture: 'clawbackAccount.json' }
+    ).as('clawbackAccount request');
+
     cy.visit('http://localhost:5173/recipe/clawback');
   });
 
-  it('should render the form', () => {
-    cy.get('#asset-code').should('exist');
-    cy.get('#issuer-secret-key').should('exist');
-    cy.get('#clawback-account').should('exist');
-    cy.get('#is-clawback-all-enabled').should('exist');
-    cy.get('#amount').should('exist');
-    cy.get('#clawback-button').should('exist');
-  });
-
-  it('should fill out the form with invalid data and show an error', () => {
-    cy.get('#asset-code').type('wrongAssetCode');
-    cy.get('#issuer-secret-key').type('WrongSecretKey');
-    cy.get('#clawback-account').type('WrongClawbackAccount');
+  it('Should performs clawback when button is clicked, and verifies the status', () => {
+    cy.get('#asset-code').type('testCoin');
+    cy.get('#issuer-secret-key').type('SB2RZHC7JIUBYHYV45VEHEYVANDY5AWHYS3IJYZHPVNJ6TJ5YPEK7OQE');
+    cy.get('#clawback-account').type('GDAM4TVOXH5RA36KIQLG6JZBM5LB2LMWTTHHJMXCZIPVORNHV4AFPQW3');
+    cy.get('#is-clawback-all-enabled').uncheck();
+    cy.get('#amount').type('100');
 
     cy.get('#clawback-button').click();
 
-    cy.get('#status').should('contain', 'Error: Error: invalid encoded string');
+    cy.get('#status').within(() => {
+      cy.contains('Transaction successful');
+      cy.get('div.flex.items-center').within(() => {
+        cy.contains('See details').should(
+          'have.attr',
+          'href',
+          'https://stellar.expert/explorer/testnet/tx/9fa82582516534232e7a9a2ccc391908790d9a2d7da6cce0a13c34e457d381dd'
+        );
+      });
+    });
+  });
+
+  it('Should try to perform clawback when button is clicked, but fails because the available balance is less than the amount', () => {
+    cy.get('#asset-code').type('testCoin');
+    cy.get('#issuer-secret-key').type('SB2RZHC7JIUBYHYV45VEHEYVANDY5AWHYS3IJYZHPVNJ6TJ5YPEK7OQE');
+    cy.get('#clawback-account').type('GDAM4TVOXH5RA36KIQLG6JZBM5LB2LMWTTHHJMXCZIPVORNHV4AFPQW3');
+    cy.get('#is-clawback-all-enabled').uncheck();
+    cy.get('#amount').type('10000000');
+
+    cy.get('#clawback-button').click();
+
+    cy.get('#status').should(
+      'contain',
+      'Error: Error: The amount for clawback (10000000) is greater than the available balance (999900.0000000)'
+    );
   });
 });
