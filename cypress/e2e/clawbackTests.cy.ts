98d4112da14a6319325637d41b9df77f603a3ddc
Joaquin Akerman <55959401+JoaquinAkerman@users.noreply.github.com>
2024-01-13 07:08:07 -0300

    Refactor Cypress tests for Clawback Asset Page and add new test
    This commit refactors the Cypress tests for the Clawback Asset Page to use variables instead of repeated string literals.
    In addition, a new test has been added to check full clawback operation

--- cypress/e2e/clawbackTests.cy.ts
+++ cypress/e2e/clawbackTests.cy.ts
@@ -1,25 +1,29 @@
-describe('Test Asset Page', () => {
+const ASSET_CODE_FOR_CLAWBACK = 'testCoin';
+const ISSUER_PUBLIC_KEY = 'GANFMU7SHOFXUM6OF32BGKADD6QSEEP25E2ZVJ6PTJUKI4WORZY6Y6SI';
+const ISSUER_SECRET_KEY = 'SB2RZHC7JIUBYHYV45VEHEYVANDY5AWHYS3IJYZHPVNJ6TJ5YPEK7OQE';
+const CLAWBACK_ACCOUNT_PUBLIC_KEY = 'GDAM4TVOXH5RA36KIQLG6JZBM5LB2LMWTTHHJMXCZIPVORNHV4AFPQW3';
+const TRANSACTION_ID = '9fa82582516534232e7a9a2ccc391908790d9a2d7da6cce0a13c34e457d381dd';
+const HORIZON_SERVER = 'https://horizon-testnet.stellar.org';
+const STELLAR_EXPERT_TESTNET_EXPLORER_SERVER = 'https://stellar.expert/explorer/testnet';
+
+describe('Test Clawback Page', () => {
   beforeEach(() => {
-    cy.intercept(
-      'GET',
-      'https://horizon-testnet.stellar.org/accounts/GANFMU7SHOFXUM6OF32BGKADD6QSEEP25E2ZVJ6PTJUKI4WORZY6Y6SI',
-      { fixture: 'issuerAccount.json' }
-    ).as('load Issuer Account request');
+    cy.intercept('GET', `${HORIZON_SERVER}/accounts/${ISSUER_PUBLIC_KEY}`, {
+      fixture: 'issuerAccount.json'
+    }).as('load Issuer Account request');
 
     cy.intercept('POST', '**/transactions', { fixture: 'transaction.json' }).as('performClawback');
-    cy.intercept(
-      'GET',
-      'https://horizon-testnet.stellar.org/accounts/GDAM4TVOXH5RA36KIQLG6JZBM5LB2LMWTTHHJMXCZIPVORNHV4AFPQW3',
-      { fixture: 'clawbackAccount.json' }
-    ).as('clawbackAccount request');
+    cy.intercept('GET', `${HORIZON_SERVER}/accounts/${CLAWBACK_ACCOUNT_PUBLIC_KEY}`, {
+      fixture: 'clawbackAccount.json'
+    }).as('load clawbackAccount request');
 
     cy.visit('http://localhost:5173/recipe/clawback');
   });
 
-  it('Should performs clawback when button is clicked, and verifies the status', () => {
-    cy.get('#asset-code').should('be.visible').type('testCoin');
-    cy.get('#issuer-secret-key').should('be.visible').type('SB2RZHC7JIUBYHYV45VEHEYVANDY5AWHYS3IJYZHPVNJ6TJ5YPEK7OQE');
-    cy.get('#clawback-account').should('be.visible').type('GDAM4TVOXH5RA36KIQLG6JZBM5LB2LMWTTHHJMXCZIPVORNHV4AFPQW3');
+  it('Should performs clawback of 100 testCoin when button is clicked, and verifies the status', () => {
+    cy.get('#asset-code').should('be.visible').type(ASSET_CODE_FOR_CLAWBACK);
+    cy.get('#issuer-secret-key').should('be.visible').type(ISSUER_SECRET_KEY);
+    cy.get('#clawback-account').should('be.visible').type(CLAWBACK_ACCOUNT_PUBLIC_KEY);
     cy.get('#is-clawback-all-enabled').should('be.visible').uncheck();
     cy.get('#amount').should('be.visible').type('100');
 
@@ -31,16 +35,36 @@ describe('Test Asset Page', () => {
         cy.contains('See details').should(
           'have.attr',
           'href',
-          'https://stellar.expert/explorer/testnet/tx/9fa82582516534232e7a9a2ccc391908790d9a2d7da6cce0a13c34e457d381dd'
+          `${STELLAR_EXPERT_TESTNET_EXPLORER_SERVER}/tx/${TRANSACTION_ID}`
+        );
+      });
+    });
+  });
+
+  it('Should perform a full clawback when checkbox is ticked', () => {
+    cy.get('#asset-code').should('be.visible').type(ASSET_CODE_FOR_CLAWBACK);
+    cy.get('#issuer-secret-key').should('be.visible').type(ISSUER_SECRET_KEY);
+    cy.get('#clawback-account').should('be.visible').type(CLAWBACK_ACCOUNT_PUBLIC_KEY);
+    cy.get('#is-clawback-all-enabled').should('be.visible').check();
+
+    cy.get('#clawback-button').click();
+
+    cy.get('#status').within(() => {
+      cy.contains('Transaction successful');
+      cy.get('div.flex.items-center').within(() => {
+        cy.contains('See details').should(
+          'have.attr',
+          'href',
+          `${STELLAR_EXPERT_TESTNET_EXPLORER_SERVER}/tx/${TRANSACTION_ID}`
         );
       });
     });
   });
 
   it('Should try to perform clawback when button is clicked, but fails because the available balance is less than the amount', () => {
-    cy.get('#asset-code').should('be.visible').type('testCoin');
-    cy.get('#issuer-secret-key').should('be.visible').type('SB2RZHC7JIUBYHYV45VEHEYVANDY5AWHYS3IJYZHPVNJ6TJ5YPEK7OQE');
-    cy.get('#clawback-account').should('be.visible').type('GDAM4TVOXH5RA36KIQLG6JZBM5LB2LMWTTHHJMXCZIPVORNHV4AFPQW3');
+    cy.get('#asset-code').should('be.visible').type(ASSET_CODE_FOR_CLAWBACK);
+    cy.get('#issuer-secret-key').should('be.visible').type(ISSUER_SECRET_KEY);
+    cy.get('#clawback-account').should('be.visible').type(CLAWBACK_ACCOUNT_PUBLIC_KEY);
     cy.get('#is-clawback-all-enabled').should('be.visible').uncheck();
     cy.get('#amount').should('be.visible').type('10000000');
 
