bf9c784dc423e0e3e2aa142aeee52b9307f82e41
Joaquin Akerman <55959401+JoaquinAkerman@users.noreply.github.com>
2024-01-16 20:52:34 -0300

    Enhance Cypress testing with data-cy attribute
    "This commit includes changes across four component files where unnecessary id attributes were removed and data-cy attributes were added to improve Cypress testing practices."

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -1,10 +1,9 @@
 <script lang="ts">
   export let title: string;
   export let content = '';
-  export let id: string;
 </script>
 
-<div {id} class="border-4 border-black w-72 mx-2 bg-gray-100 shadow-lg mt-5 rounded overflow-auto h-600px">
+<div class="border-4 border-black w-72 mx-2 bg-gray-100 shadow-lg mt-5 rounded overflow-auto h-600px">
   <h1 class="text-xl font-bold mb-5 bg-indigo-500 text-white p-5">{title}</h1>
   <div class="p-2">
     <p>{content}</p>
