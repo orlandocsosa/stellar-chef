889e0ddc08f29f6e8ab6c80394156fd962009ec4
Joaquin Akerman <55959401+JoaquinAkerman@users.noreply.github.com>
2024-01-16 15:30:46 -0300

    Declare $env/static/public module and update config
    This commit includes the declaration of the $env/static/public module in the svelte-env.d.ts file and updates the TypeScript and ESLint configurations to recognize this module.

--- src/env.d.ts
+++
@@ -1,6 +0,0 @@
-interface ImportMeta {
-  env: {
-    VITE_STELLAR_NETWORK_URL: string;
-    VITE_STELLAR_NETWORK_PASSPHRASE: string;
-  };
-}
---
+++ svelte-env.d.ts
@@ -0,0 +1,4 @@
+declare module '$env/static/public' {
+  export const PUBLIC_STELLAR_NETWORK_URL: string;
+  export const PUBLIC_STELLAR_NETWORK_PASSPHRASE: string;
+}
