ceecad6d72d1c0869f1828dde5291e3aead35da0
Joaquin Akerman <55959401+JoaquinAkerman@users.noreply.github.com>
2024-01-17 16:21:19 -0300

    Add getByDataTestAttribute command to Cypress
    Add a new Cypress command, getByDataTestAttribute, to simplify the selection of elements by their data-cy attribute.

--- cypress/e2e/asset-issuance.cy.ts
+++ cypress/e2e/asset-issuance.cy.ts
@@ -1,5 +1,5 @@
 const ASSET_CODE = 'testAsset';
-const STATUS_SELECTOR = '#status';
+const STATUS_SELECTOR = '[data-cy=status]';
 const TIMEOUT = { timeout: 35000 };
 
 const visitAssetIssuancePage = (): void => {
@@ -7,40 +7,42 @@ const visitAssetIssuancePage = (): void => {
 };
 
 const createAsset = (assetCode: string): void => {
-  cy.get('#asset-code').as('assetCodeInput').type(assetCode);
-  cy.get('#prepare-button').click();
+  cy.getByDataTestAttribute('asset-code-input').as('assetCodeInput').type(assetCode);
+  cy.getByDataTestAttribute('prepare-button').click();
 };
 
-const checkPublicKeyAndSecretKeyFormat = (publicKeyElement: string, secretKeyElement: string): void => {
-  cy.get(publicKeyElement).should('not.be.empty').invoke('text').should('match', /^G/).and('have.length', 56);
-  cy.get(secretKeyElement).should('not.be.empty').invoke('text').should('match', /^S/).and('have.length', 56);
-};
-
-const checkCoinInfoLink = (assetCode: string): void => {
-  let issuerPublicKey: string;
-
-  cy.get('#issuerPublicKey')
+const checkKeyFormat = (container: string, keyType: string, index?: number): void => {
+  const containerSelector = index !== undefined ? `${container}-${index}` : container;
+  cy.getByDataTestAttribute(`${containerSelector}-container`)
+    .find(`[for="${containerSelector}${keyType}Key"] div`)
     .invoke('text')
-    .then((text) => {
-      issuerPublicKey = text;
-    })
-    .then(() => {
-      cy.get('#coinInfo')
-        .find('a')
-        .should('have.attr', 'href')
-        .and('equal', `https://stellar.expert/explorer/testnet/asset/${assetCode}-${issuerPublicKey}`);
+    .should('match', new RegExp(`^${keyType === 'Public' ? 'G' : 'S'}`))
+    .and('have.length', 56);
+};
+const checkCoinInfoLink = (assetCode: string): void => {
+  cy.getByDataTestAttribute('coin-info')
+    .find('a')
+    .invoke('attr', 'href')
+    .then((href: string) => {
+      const urlParts = href.split('/');
+      const lastPart = urlParts[urlParts.length - 1];
+      const [actualAssetCode, actualPublicKey] = lastPart.split('-');
+
+      expect(actualAssetCode).to.equal(assetCode);
+      expect(actualPublicKey).to.match(/^G/);
+      expect(actualPublicKey).to.have.length(56);
     });
 };
-
 const checkAccountInfoLink = (publicKeyElement: string, linkDetailsId: string): void => {
   cy.get(publicKeyElement)
     .invoke('text')
     .then((accountId) => {
-      cy.get(`#${linkDetailsId}`)
+      cy.get(linkDetailsId)
         .should('have.attr', 'href')
         .and('equal', `https://stellar.expert/explorer/testnet/account/${accountId}`);
     });
 };
+
 describe('Asset Creation', () => {
   beforeEach(visitAssetIssuancePage);
 
@@ -53,17 +55,17 @@ describe('Asset Creation', () => {
     );
 
     checkCoinInfoLink(ASSET_CODE);
+    checkKeyFormat('issuer', 'Public');
+    checkKeyFormat('issuer', 'Secret');
+    checkAccountInfoLink('label[for="issuerPublicKey"] div', 'h3 a');
 
-    checkPublicKeyAndSecretKeyFormat('#issuerPublicKey', '#issuerSecretKey');
-    checkAccountInfoLink('#issuerPublicKey', 'issuerDetailsLink');
-
-    checkPublicKeyAndSecretKeyFormat('#distributorPublicKey', '#distributorSecretKey');
-    checkAccountInfoLink('#distributorPublicKey', 'distributorDetailsLink');
+    checkKeyFormat('distributor', 'Public');
+    checkKeyFormat('distributor', 'Secret');
   });
 
   it('Should create a new asset with "frozen" and "clawback" options enabled, verifies the keys, and the coin info link.', () => {
-    cy.get('#frozen-asset').check();
-    cy.get('#clawback-enabled').check();
+    cy.getByDataTestAttribute('frozen-asset').check();
+    cy.getByDataTestAttribute('clawback-enabled').check();
     createAsset(ASSET_CODE);
 
     cy.get(STATUS_SELECTOR, TIMEOUT).should(
@@ -71,18 +73,19 @@ describe('Asset Creation', () => {
       `Transaction successful. Distributor account balance: 1000000.0000000 ${ASSET_CODE}`
     );
 
-    checkPublicKeyAndSecretKeyFormat('#issuerPublicKey', '#issuerSecretKey');
-    checkAccountInfoLink('#issuerPublicKey', 'issuerDetailsLink');
-
-    checkPublicKeyAndSecretKeyFormat('#distributorPublicKey', '#distributorSecretKey');
-    checkAccountInfoLink('#distributorPublicKey', 'distributorDetailsLink');
     checkCoinInfoLink(ASSET_CODE);
+    checkKeyFormat('issuer', 'Public');
+    checkKeyFormat('issuer', 'Secret');
+    checkAccountInfoLink('label[for="issuerPublicKey"] div', 'h3 a');
+
+    checkKeyFormat('distributor', 'Public');
+    checkKeyFormat('distributor', 'Secret');
   });
 
   it('Should create a new asset with frozen and clawback options and 1 holder. Checks the details, keys and links, including holder', () => {
-    cy.get('#frozen-asset').check();
-    cy.get('#clawback-enabled').check();
-    cy.get('#create-holders').check();
+    cy.getByDataTestAttribute('frozen-asset').check();
+    cy.getByDataTestAttribute('clawback-enabled').check();
+    cy.getByDataTestAttribute('create-holders').check();
     createAsset(ASSET_CODE);
 
     cy.get(STATUS_SELECTOR, TIMEOUT).should(
@@ -91,23 +94,24 @@ describe('Asset Creation', () => {
     );
 
     checkCoinInfoLink(ASSET_CODE);
+    checkKeyFormat('issuer', 'Public');
+    checkKeyFormat('issuer', 'Secret');
+    checkAccountInfoLink('label[for="issuerPublicKey"] div', 'h3 a');
 
-    checkPublicKeyAndSecretKeyFormat('#issuerPublicKey', '#issuerSecretKey');
-    checkAccountInfoLink('#issuerPublicKey', 'issuerDetailsLink');
+    checkKeyFormat('distributor', 'Public');
+    checkKeyFormat('distributor', 'Secret');
 
-    checkPublicKeyAndSecretKeyFormat('#distributorPublicKey', '#distributorSecretKey');
-    checkAccountInfoLink('#distributorPublicKey', 'distributorDetailsLink');
+    cy.getByDataTestAttribute('toggle-holders-button').click();
 
-    cy.get('#toggle-holders-button').click();
-
-    checkPublicKeyAndSecretKeyFormat('#holder1PublicKey', '#holder1SecretKey');
-    checkAccountInfoLink('#holder1PublicKey', 'holder1DetailsLink');
+    checkKeyFormat('holder', 'Public', 1);
+    checkKeyFormat('holder', 'Secret', 1);
   });
 
   it('Should handle asset dsitribution failure with not enough funds for the holders', () => {
-    cy.get('#create-holders').check();
-    cy.get('#number-of-holders').clear().type('4');
-    cy.get('#payment-amount').clear().type('100');
+    cy.getByDataTestAttribute('distributor-payment-amount-input').clear().type('100');
+    cy.getByDataTestAttribute('create-holders').check();
+    cy.getByDataTestAttribute('number-of-holders-input').clear().type('4');
+    cy.getByDataTestAttribute('balance-per-holder-input').clear().type('1000');
     createAsset(ASSET_CODE);
 
     cy.get(STATUS_SELECTOR, TIMEOUT).should(
