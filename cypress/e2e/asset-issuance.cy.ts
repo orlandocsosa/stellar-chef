4b22d99af54b79e17a9dcf20be82557b1bf94cc1
Joaquin Akerman <55959401+JoaquinAkerman@users.noreply.github.com>
2024-01-18 12:14:41 -0300

    Update tests
    Update tests for assertions based on data-cy on anchor elements

--- cypress/e2e/asset-issuance.cy.ts
+++ cypress/e2e/asset-issuance.cy.ts
@@ -1,110 +1,97 @@
 const ASSET_CODE = 'testAsset';
 const STATUS_SELECTOR = '[data-cy=status]';
 const TIMEOUT = { timeout: 35000 };
-
-const visitAssetIssuancePage = (): void => {
-  cy.visit('/recipe/asset-issuance');
-};
-
-const createAsset = (assetCode: string): void => {
-  cy.getByDataTestAttribute('asset-code-input').as('assetCodeInput').type(assetCode);
-  cy.getByDataTestAttribute('prepare-button').click();
-};
-
-const checkKeyFormat = (container: string, keyType: string, index?: number): void => {
-  const containerSelector = index !== undefined ? `${container}-${index}` : container;
-  cy.getByDataTestAttribute(`${containerSelector}-container`)
-    .find(`[for="${containerSelector}${keyType}Key"] div`)
-    .invoke('text')
-    .should('match', new RegExp(`^${keyType === 'Public' ? 'G' : 'S'}`))
-    .and('have.length', 56);
-};
-const checkCoinInfoLink = (assetCode: string): void => {
-  cy.getByDataTestAttribute('coin-info')
-    .find('a')
-    .invoke('attr', 'href')
-    .then((href: string) => {
-      const urlParts = href.split('/');
-      const lastPart = urlParts[urlParts.length - 1];
-      const [actualAssetCode, actualPublicKey] = lastPart.split('-');
-
-      expect(actualAssetCode).to.equal(assetCode);
-      expect(actualPublicKey).to.match(/^G/);
-      expect(actualPublicKey).to.have.length(56);
-    });
-};
-const checkAccountInfoLink = (publicKeyElement: string, linkDetailsId: string): void => {
-  cy.get(publicKeyElement)
-    .invoke('text')
-    .then((accountId) => {
-      cy.get(linkDetailsId)
-        .should('have.attr', 'href')
-        .and('equal', `https://stellar.expert/explorer/testnet/account/${accountId}`);
-    });
-};
+const EXPECTED_STELLAR_EXPERT_ASSET_URL = 'https://stellar.expert/explorer/testnet/asset/testAsset';
+const EXPECTED_STELLAR_EXPERT_ACCOUNT_URL = 'https://stellar.expert/explorer/testnet/account/';
 
 describe('Asset Creation', () => {
-  beforeEach(visitAssetIssuancePage);
-
-  it('Should create a new asset and verifies the details links, public and secret keys for the issuer and distributor, and the coin info link', () => {
-    createAsset(ASSET_CODE);
-
-    cy.get(STATUS_SELECTOR, TIMEOUT).should(
-      'contain',
-      `Transaction successful. Distributor account balance: 1000000.0000000 ${ASSET_CODE}`
-    );
-
-    checkCoinInfoLink(ASSET_CODE);
-    checkKeyFormat('issuer', 'Public');
-    checkKeyFormat('issuer', 'Secret');
-    checkAccountInfoLink('label[for="issuerPublicKey"] div', 'h3 a');
-
-    checkKeyFormat('distributor', 'Public');
-    checkKeyFormat('distributor', 'Secret');
+  beforeEach(() => {
+    cy.visit('/recipe/asset-issuance');
+    cy.getByDataTestAttribute('asset-code-input').type(ASSET_CODE);
   });
 
-  it('Should create a new asset with "frozen" and "clawback" options enabled, verifies the keys, and the coin info link.', () => {
-    cy.getByDataTestAttribute('frozen-asset').check();
-    cy.getByDataTestAttribute('clawback-enabled').check();
-    createAsset(ASSET_CODE);
+  it('Should create a new asset and verifies the details links, the coin info link and the status', () => {
+    cy.getByDataTestAttribute('prepare-button').click();
 
     cy.get(STATUS_SELECTOR, TIMEOUT).should(
       'contain',
       `Transaction successful. Distributor account balance: 1000000.0000000 ${ASSET_CODE}`
     );
 
-    checkCoinInfoLink(ASSET_CODE);
-    checkKeyFormat('issuer', 'Public');
-    checkKeyFormat('issuer', 'Secret');
-    checkAccountInfoLink('label[for="issuerPublicKey"] div', 'h3 a');
+    cy.getByDataTestAttribute('issuer-info-link')
+      .should('be.visible')
+      .should('have.prop', 'href')
+      .and('include', EXPECTED_STELLAR_EXPERT_ACCOUNT_URL);
 
-    checkKeyFormat('distributor', 'Public');
-    checkKeyFormat('distributor', 'Secret');
+    cy.getByDataTestAttribute('distributor-info-link')
+      .should('be.visible')
+      .should('have.prop', 'href')
+      .and('include', EXPECTED_STELLAR_EXPERT_ACCOUNT_URL);
+
+    cy.getByDataTestAttribute('coin-info-link')
+      .should('be.visible')
+      .should('have.prop', 'href')
+      .and('include', EXPECTED_STELLAR_EXPERT_ASSET_URL);
+  });
+
+  it('Should create a new asset with "frozen" and "clawback" options enabled, verifies the details links, the coin info link, and the status', () => {
+    cy.getByDataTestAttribute('frozen-asset').check();
+    cy.getByDataTestAttribute('clawback-enabled').check();
+    cy.getByDataTestAttribute('prepare-button').click();
+
+    cy.get(STATUS_SELECTOR, TIMEOUT).should(
+      'contain',
+      `Transaction successful. Distributor account balance: 1000000.0000000 ${ASSET_CODE}`
+    );
+
+    cy.getByDataTestAttribute('issuer-info-link')
+      .should('be.visible')
+      .should('have.prop', 'href')
+      .and('include', EXPECTED_STELLAR_EXPERT_ACCOUNT_URL);
+
+    cy.getByDataTestAttribute('distributor-info-link')
+      .should('be.visible')
+      .should('have.prop', 'href')
+      .and('include', EXPECTED_STELLAR_EXPERT_ACCOUNT_URL);
+
+    cy.getByDataTestAttribute('coin-info-link')
+      .should('be.visible')
+      .should('have.prop', 'href')
+      .and('include', EXPECTED_STELLAR_EXPERT_ASSET_URL);
   });
 
   it('Should create a new asset with frozen and clawback options and 1 holder. Checks the details, keys and links, including holder', () => {
     cy.getByDataTestAttribute('frozen-asset').check();
     cy.getByDataTestAttribute('clawback-enabled').check();
     cy.getByDataTestAttribute('create-holders').check();
-    createAsset(ASSET_CODE);
+    cy.getByDataTestAttribute('prepare-button').click();
 
     cy.get(STATUS_SELECTOR, TIMEOUT).should(
       'contain',
       `Transaction successful. Distributor account balance: 999900.0000000 ${ASSET_CODE}`
     );
 
-    checkCoinInfoLink(ASSET_CODE);
-    checkKeyFormat('issuer', 'Public');
-    checkKeyFormat('issuer', 'Secret');
-    checkAccountInfoLink('label[for="issuerPublicKey"] div', 'h3 a');
+    cy.getByDataTestAttribute('issuer-info-link')
+      .should('be.visible')
+      .should('have.prop', 'href')
+      .and('include', EXPECTED_STELLAR_EXPERT_ACCOUNT_URL);
 
-    checkKeyFormat('distributor', 'Public');
-    checkKeyFormat('distributor', 'Secret');
+    cy.getByDataTestAttribute('distributor-info-link')
+      .should('be.visible')
+      .should('have.prop', 'href')
+      .and('include', EXPECTED_STELLAR_EXPERT_ACCOUNT_URL);
+
+    cy.getByDataTestAttribute('coin-info-link')
+      .should('be.visible')
+      .should('have.prop', 'href')
+      .and('include', EXPECTED_STELLAR_EXPERT_ASSET_URL);
 
     cy.getByDataTestAttribute('toggle-holders-button').click();
 
-    checkKeyFormat('holder', 'Public', 1);
-    checkKeyFormat('holder', 'Secret', 1);
+    cy.getByDataTestAttribute('holder-1-info-link')
+      .should('be.visible')
+      .should('have.prop', 'href')
+      .and('include', EXPECTED_STELLAR_EXPERT_ACCOUNT_URL);
   });
 
   it('Should handle asset dsitribution failure with not enough funds for the holders', () => {
@@ -112,7 +99,7 @@ describe('Asset Creation', () => {
     cy.getByDataTestAttribute('create-holders').check();
     cy.getByDataTestAttribute('number-of-holders-input').clear().type('4');
     cy.getByDataTestAttribute('balance-per-holder-input').clear().type('1000');
-    createAsset(ASSET_CODE);
+    cy.getByDataTestAttribute('prepare-button').click();
 
     cy.get(STATUS_SELECTOR, TIMEOUT).should(
       'contain',
