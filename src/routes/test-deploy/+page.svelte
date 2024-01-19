4451078158f8be63713b08350dcb0952c75ae7e5
eloptimistadelcode <153465933+eloptimistadelcode@users.noreply.github.com>
2024-01-18 22:15:07 -0300

    Only use stellar-sdk package to submit a transaction

--- src/routes/test-deploy/+page.svelte
+++ src/routes/test-deploy/+page.svelte
@@ -1,20 +1,25 @@
 <script>
-  import { Asset, Keypair, Operation } from 'stellar-sdk';
-  import { buildTransaction, server } from '../../services/stellar/utils';
+  import { Asset, Horizon, Keypair, Networks, Operation, TransactionBuilder } from 'stellar-sdk';
 
   async function prepare() {
+    const server = new Horizon.Server('https://horizon-testnet.stellar.org');
     const account1Keypair = Keypair.random();
     const account2Keypair = Keypair.random();
     await server.friendbot(account1Keypair.publicKey()).call();
     await server.friendbot(account2Keypair.publicKey()).call();
+    const paymentOp = Operation.payment({
+      amount: '100',
+      asset: Asset.native(),
+      destination: account2Keypair.publicKey()
+    });
 
-    const transaction = buildTransaction(await server.loadAccount(account1Keypair.publicKey()), [
-      Operation.payment({
-        amount: '100',
-        asset: Asset.native(),
-        destination: account2Keypair.publicKey()
-      })
-    ]);
+    const transaction = new TransactionBuilder(await server.loadAccount(account1Keypair.publicKey()), {
+      networkPassphrase: Networks.TESTNET,
+      fee: '100'
+    })
+      .setTimeout(30)
+      .addOperation(paymentOp)
+      .build();
 
     transaction.sign(Keypair.fromSecret(account1Keypair.secret()));
     await server.submitTransaction(transaction);
