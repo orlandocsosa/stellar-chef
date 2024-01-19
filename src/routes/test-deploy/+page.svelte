f1e37f560bda715d25a0502fb50fcfa29eca45b9
eloptimistadelcode <153465933+eloptimistadelcode@users.noreply.github.com>
2024-01-18 22:09:47 -0300

    Create accounts without using the Account class

--- src/routes/test-deploy/+page.svelte
+++ src/routes/test-deploy/+page.svelte
@@ -1,21 +1,22 @@
 <script>
-  import { Asset, Keypair, Operation, TransactionBuilder } from 'stellar-sdk';
-  import { Account } from '../../services/stellar/Account';
+  import { Asset, Keypair, Operation } from 'stellar-sdk';
   import { buildTransaction, server } from '../../services/stellar/utils';
 
   async function prepare() {
-    const account1 = await Account.create().fundWithFriendBot();
-    const account2 = await Account.create().fundWithFriendBot();
+    const account1Keypair = Keypair.random();
+    const account2Keypair = Keypair.random();
+    await server.friendbot(account1Keypair.publicKey()).call();
+    await server.friendbot(account2Keypair.publicKey()).call();
 
-    const transaction = buildTransaction(await server.loadAccount(account1.publicKey), [
+    const transaction = buildTransaction(await server.loadAccount(account1Keypair.publicKey()), [
       Operation.payment({
         amount: '100',
         asset: Asset.native(),
-        destination: account2.publicKey
+        destination: account2Keypair.publicKey()
       })
     ]);
 
-    transaction.sign(Keypair.fromSecret(account1.secretKey));
+    transaction.sign(Keypair.fromSecret(account1Keypair.secret()));
     await server.submitTransaction(transaction);
   }
 </script>
