603c0f59474e3c9504b01662a70602057e47faa1
eloptimistadelcode <153465933+eloptimistadelcode@users.noreply.github.com>
2024-01-18 22:19:29 -0300

    Add failing and working submits to compare

--- src/routes/test-deploy/+page.svelte
+++ src/routes/test-deploy/+page.svelte
@@ -1,7 +1,8 @@
 <script>
   import { Asset, Horizon, Keypair, Networks, Operation, TransactionBuilder } from 'stellar-sdk';
+  import { buildTransaction, server } from '../../services/stellar/utils';
 
-  async function prepare() {
+  async function workingSubmit() {
     const server = new Horizon.Server('https://horizon-testnet.stellar.org');
     const account1Keypair = Keypair.random();
     const account2Keypair = Keypair.random();
@@ -23,6 +24,29 @@
 
     transaction.sign(Keypair.fromSecret(account1Keypair.secret()));
     await server.submitTransaction(transaction);
+  }
+
+  async function failingSubmit() {
+    const account1Keypair = Keypair.random();
+    const account2Keypair = Keypair.random();
+    await server.friendbot(account1Keypair.publicKey()).call();
+    await server.friendbot(account2Keypair.publicKey()).call();
+
+    const transaction = buildTransaction(await server.loadAccount(account1Keypair.publicKey()), [
+      Operation.payment({
+        amount: '100',
+        asset: Asset.native(),
+        destination: account2Keypair.publicKey()
+      })
+    ]);
+
+    transaction.sign(Keypair.fromSecret(account1Keypair.secret()));
+    await server.submitTransaction(transaction);
+  }
+
+  async function prepare() {
+    await workingSubmit();
+    await failingSubmit();
   }
 </script>
 
