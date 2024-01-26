e7ffe4fa9e050d782873d311f55da3d784a377c7
Joaquin Akerman <55959401+JoaquinAkerman@users.noreply.github.com>
2024-01-26 19:02:45 -0300

    Add unfreezeAsset function
    Added the unfreezeAsset function to handle asset unfreezing operations. Also integrated a toggle switch to control the freeze/unfreeze state of the asset.

--- src/routes/recipe/freeze-assets/+page.svelte
+++ src/routes/recipe/freeze-assets/+page.svelte
@@ -7,6 +7,7 @@
   import Card from '../../../components/Card.svelte';
   import Input from '../../../components/Input.svelte';
   import Button from '../../../components/Button.svelte';
+  import Switch from '../../../components/Switch.svelte';
 
   const assetService = new AssetStorageService();
   let assetsOnLocalStorage = assetService.getAll();
@@ -17,6 +18,7 @@
   let status = '';
   let isTransactionSuccessful = false;
   let transactionHash = '';
+  let shouldFreezeAsset = true;
 
   $: {
     const selectedAsset = assetsOnLocalStorage.find((asset) => asset.code === assetCode);
@@ -43,7 +45,7 @@
         console.error('Failed to load account:', error);
         return;
       }
-
+      status = 'Setting freeze flags...';
       const operations = [];
 
       operations.push(
@@ -59,13 +61,15 @@
           authorize: false
         })
       );
-
+      status = 'Performing transaction...';
       let transaction = buildTransaction(sourceAccount, operations);
 
       transaction.sign(issuerKeypair);
 
       const transactionResult = await submitTransaction(transaction);
       isTransactionSuccessful = transactionResult.successful;
+
+      status = `Transaction ${isTransactionSuccessful ? 'succeeded' : 'failed'}.`;
 
       if (isTransactionSuccessful) {
         transactionHash = transactionResult.hash;
@@ -81,13 +85,76 @@
     }
   }
 
+  async function unfreezeAsset() {
+    isLoading = true;
+    status = '';
+    isTransactionSuccessful = false;
+    transactionHash = '';
+    try {
+      status = 'Unfreezing asset...';
+      const issuerKeypair = Keypair.fromSecret(issuerSecretKey);
+      const issuerPublicKey = issuerKeypair.publicKey();
+
+      let sourceAccount;
+      let assetHolder;
+      try {
+        sourceAccount = await server.loadAccount(issuerPublicKey);
+        assetHolder = await server.loadAccount(assetHolderPublicKey);
+      } catch (error) {
+        status = `Failed to load account: ${String(error)}`;
+        console.error('Failed to load account:', error);
+        return;
+      }
+      status = 'Clearing freeze flags...';
+      const operations = [];
+
+      operations.push(
+        Operation.setOptions({
+          clearFlags: 1
+        }),
+        Operation.setOptions({
+          clearFlags: 2
+        }),
+        Operation.allowTrust({
+          trustor: assetHolderPublicKey,
+          assetCode: assetCode,
+          authorize: true
+        })
+      );
+      status = 'Performing transaction...';
+      let transaction = buildTransaction(sourceAccount, operations);
+
+      transaction.sign(issuerKeypair);
+
+      const transactionResult = await submitTransaction(transaction);
+      isTransactionSuccessful = transactionResult.successful;
+
+      status = `Transaction ${transactionResult.successful ? 'succeeded' : 'failed'}.`;
+
+      if (isTransactionSuccessful) {
+        transactionHash = transactionResult.hash;
+        status = `Asset unfrozen successfully! `;
+      }
+    } catch (error) {
+      status = `Error: ${String(error)}`;
+      console.error('An error occurred:', error);
+      isTransactionSuccessful = false;
+      isLoading = false;
+    } finally {
+      isLoading = false;
+    }
+  }
+
   function allowOnlyAlphanumeric(inputValue: string) {
     return inputValue.replace(/[^a-zA-Z0-9]/g, '');
+  }
+  function performAssetAction() {
+    return shouldFreezeAsset ? freezeAsset() : unfreezeAsset();
   }
 </script>
 
 <div class="flex justify-center">
-  <form class="flex flex-col" on:submit|preventDefault={freezeAsset}>
+  <form class="flex flex-col" on:submit|preventDefault={performAssetAction}>
     <Card title="Freeze Asset">
       <div class="flex flex-col">
         <label for="asset-code-select">
@@ -131,17 +198,26 @@
           handleInput={allowOnlyAlphanumeric}
         />
 
-        <div class="flex flex-col items-center">
-          <Button dataCy="freeze-button" label={isLoading ? 'Freezing...' : 'Freeze Asset'} disabled={isLoading} />
-
-          <div data-cy="status" class="min-h-[50px] overflow-auto mt-4 w-full">
-            <p>{status}</p>
-            {#if isTransactionSuccessful}
-              <p>
-                <TransactionInfo {transactionHash} />
-              </p>
-            {/if}
+        <div class="flex flex-col items-center justify-center">
+          <div class="flex items-center m-2">
+            <Switch bind:checked={shouldFreezeAsset} />
+            <p class="ml-3">{shouldFreezeAsset ? 'Freeze' : 'Unfreeze'}</p>
           </div>
+          <Button
+            dataCy="perform-button"
+            label={isLoading ? (shouldFreezeAsset ? 'Freezing Asset...' : 'Unfreezing Asset...') : 'Perform'}
+            disabled={isLoading}
+            on:click={performAssetAction}
+          />
+        </div>
+
+        <div data-cy="status" class="min-h-[50px] overflow-auto mt-4 w-full justify-center">
+          <p>{status}</p>
+          {#if isTransactionSuccessful}
+            <p>
+              <TransactionInfo {transactionHash} />
+            </p>
+          {/if}
         </div>
       </div></Card
     >
