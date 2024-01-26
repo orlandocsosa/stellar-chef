5b23b64b1e8bd21c4ea8f4632df3577dcac10ec6
Joaquin Akerman <55959401+JoaquinAkerman@users.noreply.github.com>
2024-01-25 21:58:02 -0300

    Add logic for freezing assets
    Added logic to the freezeAsset function, which builds, signs, and submits a transaction to freeze an asset. Also, the transactionLink is now displayed in the status div upon successful transaction.

--- src/routes/recipe/freeze-assets/+page.svelte
+++ src/routes/recipe/freeze-assets/+page.svelte
@@ -1,5 +1,10 @@
 <script lang="ts">
+  import { Operation, Asset, Keypair } from 'stellar-sdk';
+
+  import { Account } from '../../../services/stellar/Account';
   import AssetStorageService from '../../../services/asset/Asset';
+  import { buildTransaction, server, submitTransaction } from '../../../services/stellar/utils';
+  import { prepareFreezeAssetTransaction } from '../../../services/stellar/transactions/prepareFreezeAssetTransaction';
 
   import Card from '../../../components/Card.svelte';
   import Input from '../../../components/Input.svelte';
@@ -11,13 +16,75 @@
   let issuerSecretKey = '';
   let assetHolderPublicKey = '';
   let isLoading = false;
+  let status = '';
+  let transactionLink = '';
 
   $: {
     const selectedAsset = assetsOnLocalStorage.find((asset) => asset.code === assetCode);
     issuerSecretKey = selectedAsset ? selectedAsset.issuerSecret : '';
   }
 
-  async function freezeAsset() {}
+  async function freezeAsset() {
+    isLoading = true;
+    status = '';
+    transactionLink = '';
+    try {
+      status = 'Freezing asset...';
+      // Derive the public key from the issuer secret key
+      const issuerKeypair = Keypair.fromSecret(issuerSecretKey);
+      const issuerPublicKey = issuerKeypair.publicKey();
+
+      // Fetch the accounts from the server to be used in the transaction
+      let sourceAccount;
+      let assetHolder;
+      try {
+        sourceAccount = await server.loadAccount(issuerPublicKey);
+        assetHolder = await server.loadAccount(assetHolderPublicKey);
+      } catch (error) {
+        status = `Error: ${String(error)}`;
+        console.error('Failed to load account:', error);
+        return;
+      }
+
+      // Define operations
+      const operations = [];
+
+      // Add operations to set the AUTHORIZATION REQUIRED and AUTHORIZATION REVOCABLE flags
+      operations.push(
+        Operation.setOptions({
+          setFlags: 1 // AUTH_REQUIRED_FLAG
+        }),
+        Operation.setOptions({
+          setFlags: 2 // AUTH_REVOCABLE_FLAG
+        }),
+        Operation.allowTrust({
+          trustor: assetHolderPublicKey,
+          assetCode: assetCode,
+          authorize: false
+        })
+      );
+
+      // Build the transaction
+      let transaction = buildTransaction(sourceAccount, operations);
+
+      // Sign the transaction
+      transaction.sign(issuerKeypair);
+
+      // Submit the transaction
+      const transactionResult = await submitTransaction(transaction);
+      if (transactionResult.successful) {
+        const transactionHash = transactionResult.hash;
+        transactionLink = `https://stellar.expert/explorer/testnet/tx/${transactionHash}`;
+        status = `Asset frozen successfully! `;
+      }
+    } catch (error) {
+      status = `Error: ${String(error)}`;
+      console.error('An error occurred:', error);
+      isLoading = false;
+    } finally {
+      isLoading = false;
+    }
+  }
 
   function allowOnlyAlphanumeric(inputValue: string) {
     return inputValue.replace(/[^a-zA-Z0-9]/g, '');
@@ -55,8 +122,22 @@
 
         Asset Holder Public Key
         <Input dataCy="asset-holder-public-key-input" bind:value={assetHolderPublicKey} required disabled={isLoading} />
-
-        <Button dataCy="freeze-button" label={isLoading ? 'Freezing...' : 'Freeze Asset'} disabled={isLoading} />
+        <div>
+          <Button dataCy="freeze-button" label={isLoading ? 'Freezing...' : 'Freeze Asset'} disabled={isLoading} />
+          <div data-cy="status" class="min-h-[50px] overflow-auto mt-4">
+            <p>{status}</p>
+            {#if transactionLink}
+              <p>
+                <a
+                  data-cy="transaction-link"
+                  class="text-blue-500 hover:text-blue-800 ml-1 underline"
+                  href={transactionLink}
+                  target="_blank">View transaction</a
+                >
+              </p>
+            {/if}
+          </div>
+        </div>
       </div>
     </Card>
   </form>
