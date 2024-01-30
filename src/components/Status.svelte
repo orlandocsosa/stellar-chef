d7b7637de3174961ed640cf97adea43e1451e1ef
Joaquin Akerman <55959401+JoaquinAkerman@users.noreply.github.com>
2024-01-30 11:39:22 -0300

    Implement Status Component
     Refactored the status message display into a separate reusable Svelte component, Status. This component takes in the status message, a success boolean, and a transaction hash, and displays the appropriate message.

---
+++ src/components/Status.svelte
@@ -0,0 +1,16 @@
+<script>
+  import TransactionInfo from './TransactionInfo.svelte';
+
+  export let status = '';
+  export let isTransactionSuccessful = false;
+  export let transactionHash = '';
+</script>
+
+<div data-cy="status" class="min-h-[50px] overflow-auto mt-4 w-full justify-center">
+  <p>{status}</p>
+  {#if isTransactionSuccessful}
+    <p>
+      <TransactionInfo {transactionHash} />
+    </p>
+  {/if}
+</div>
