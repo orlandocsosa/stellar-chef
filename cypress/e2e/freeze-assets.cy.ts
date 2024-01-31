c993582f2393f0b58a763e74c0ac364768059bb1
Joaquin Akerman <55959401+JoaquinAkerman@users.noreply.github.com>
2024-01-31 18:00:11 -0300

    Use env vars and shared fixtures in freeze-assets tests
    This commit refactors the freeze-assets tests to use environment variables defined in cypress.config.ts. It also updates the tests to use the same fixtures as the previously updated tests.

--- cypress/e2e/freeze-assets.cy.ts
+++ cypress/e2e/freeze-assets.cy.ts
@@ -1,25 +1,28 @@
-const TEST_ASSET_CODE = 'testCoin';
-const TEST_ISSUER_PUBLIC_KEY = 'GDJ4QQ7HKBO2P44BUDCKYBFZCVGPRDOFRXLTHMFZ3DFVEQDWVGIR3WGR';
-const TEST_ISSUER_SECRET_KEY = 'SDLHQKNDTHFZM3X44VACP3ZTLAKEI2ACAVIJIH7GHAMKLZWCDWJDCUQO';
-const TEST_ASSET_HOLDER_PUBLIC_KEY = 'GDY2JIGRR5JQFXBM2CP6AAV4W3TYMI6KIAXMWEBFCGYXY2ON3Y5LRLIS';
-const FREEZE_TRANSACTION_ID = '1a33a9ca4e0fbf72c0108af38921142a9b2344923ac4d864c7c53e2f8a30b014';
+const FREEZE_ASSET_CODE: string = Cypress.env('ASSET_CODE');
+
+const FREEZE_ISSUER_PUBLIC_KEY: string = Cypress.env('ISSUER_PUBLIC_KEY');
+const FREEZE_ISSUER_SECRET_KEY: string = Cypress.env('ISSUER_SECRET_KEY');
+const FREEZE_ASSET_HOLDER_PUBLIC_KEY: string = Cypress.env('HOLDER_PUBLIC_KEY');
+const EXPECTED_STATUS_LINK_FREEZE: string = Cypress.env('EXPECTED_STATUS_LINK');
 
 describe('Asset Freezing', () => {
   beforeEach(() => {
+    cy.clearLocalStorage();
+
     cy.intercept(
       {
         method: 'GET',
-        url: `https://horizon-testnet.stellar.org/accounts/${TEST_ISSUER_PUBLIC_KEY}`
+        url: `https://horizon-testnet.stellar.org/accounts/${FREEZE_ISSUER_PUBLIC_KEY}`
       },
-      { fixture: 'freeze-assets/stellarIssuerGetResponse.json' }
+      { fixture: 'issuerAccount.json' }
     ).as('stellarIssuerGetRequest');
 
     cy.intercept(
       {
         method: 'GET',
-        url: `https://horizon-testnet.stellar.org/accounts/${TEST_ASSET_HOLDER_PUBLIC_KEY}`
+        url: `https://horizon-testnet.stellar.org/accounts/${FREEZE_ASSET_HOLDER_PUBLIC_KEY}`
       },
-      { fixture: 'freeze-assets/stellarHolderGetResponse.json' }
+      { fixture: 'holderAccount.json' }
     ).as('stellarHolderGetRequest');
 
     cy.intercept(
@@ -27,7 +30,7 @@ describe('Asset Freezing', () => {
         method: 'POST',
         url: 'https://horizon-testnet.stellar.org/transactions'
       },
-      { fixture: 'freeze-assets/stellarTransactionPostResponse.json' }
+      { fixture: 'transaction.json' }
     ).as('stellarTransactionPostRequest');
 
     cy.intercept(
@@ -37,17 +40,17 @@ describe('Asset Freezing', () => {
       },
       {
         statusCode: 400,
-        fixture: 'freeze-assets/stellarTransactionFailPostResponse.json'
+        fixture: 'transactionFail.json'
       }
     ).as('stellarWrongAccountGetRequest');
 
     cy.visit('/recipe/freeze-assets');
-    cy.getByDataTestAttribute('asset-code-input').type(TEST_ASSET_CODE);
-    cy.getByDataTestAttribute('issuer-secret-key-input').type(TEST_ISSUER_SECRET_KEY);
+    cy.getByDataTestAttribute('asset-code-input').type(FREEZE_ASSET_CODE);
+    cy.getByDataTestAttribute('issuer-secret-key-input').type(FREEZE_ISSUER_SECRET_KEY);
   });
 
   it('Should freezes an asset', () => {
-    cy.getByDataTestAttribute('asset-holder-public-key-input').type(TEST_ASSET_HOLDER_PUBLIC_KEY);
+    cy.getByDataTestAttribute('asset-holder-public-key-input').type(FREEZE_ASSET_HOLDER_PUBLIC_KEY);
     cy.getByDataTestAttribute('freeze-switch').find('input').check({ force: true });
 
     cy.getByDataTestAttribute('perform-button').click();
@@ -55,27 +58,19 @@ describe('Asset Freezing', () => {
     cy.getByDataTestAttribute('status').should('contain', 'Asset frozen successfully!');
     cy.getByDataTestAttribute('status').within(() => {
       cy.contains('Asset frozen successfully!');
-      cy.contains('See details').should(
-        'have.prop',
-        'href',
-        `https://stellar.expert/explorer/testnet/tx/${FREEZE_TRANSACTION_ID}`
-      );
+      cy.contains('See details').should('have.prop', 'href', EXPECTED_STATUS_LINK_FREEZE);
     });
   });
 
   it('Should unfreezes an asset', () => {
-    cy.getByDataTestAttribute('asset-holder-public-key-input').type(TEST_ASSET_HOLDER_PUBLIC_KEY);
+    cy.getByDataTestAttribute('asset-holder-public-key-input').type(FREEZE_ASSET_HOLDER_PUBLIC_KEY);
     cy.getByDataTestAttribute('freeze-switch').find('input').uncheck({ force: true });
     cy.getByDataTestAttribute('perform-button').click();
 
     cy.getByDataTestAttribute('status').should('contain', 'Asset unfrozen successfully!');
     cy.getByDataTestAttribute('status').within(() => {
       cy.contains('Asset unfrozen successfully!');
-      cy.contains('See details').should(
-        'have.prop',
-        'href',
-        `https://stellar.expert/explorer/testnet/tx/${FREEZE_TRANSACTION_ID}`
-      );
+      cy.contains('See details').should('have.prop', 'href', EXPECTED_STATUS_LINK_FREEZE);
     });
   });
 
