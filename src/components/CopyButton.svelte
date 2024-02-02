b8759e757ba51163cb62df6cc0cbb446b98e91e6
Joaquin Akerman <55959401+JoaquinAkerman@users.noreply.github.com>
2024-02-02 19:47:29 -0300

    Update CopyButton Component
    Updated the CopyButton.svelte component to stop event propagation when the copy button is clicked. This prevents the click event from bubbling up to parent elements and potentially triggering their click event handlers.

--- src/components/CopyButton.svelte
+++ src/components/CopyButton.svelte
@@ -2,7 +2,8 @@
   export let textToCopy: string | number;
   let copied = false;
 
-  async function copyToClipboard() {
+  async function copyToClipboard(event: MouseEvent) {
+    event.stopPropagation();
     await navigator.clipboard.writeText(textToCopy.toString());
     copied = true;
     setTimeout(() => (copied = false), 2000);
@@ -10,6 +11,7 @@
 </script>
 
 <button
+  type="button"
   on:click={copyToClipboard}
   class="ml-2 p-1 text-xs rounded text-black hover:bg-gray-200 transform translate-y-[-2px]"
 >
