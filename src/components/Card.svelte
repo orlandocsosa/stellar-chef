e91a07a61ef4c6d09a3f30a35463f913720c6de5
Joaquin Akerman <55959401+JoaquinAkerman@users.noreply.github.com>
2024-02-12 22:56:32 -0300

    Update Card component width in Card.svelte

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -3,7 +3,7 @@
   export let content = '';
 </script>
 
-<div class="border-4 border-black w-72 mx-2 bg-gray-100 shadow-lg mt-5 rounded overflow-auto min-h-[600px]">
+<div class="border-4 border-black w-[500px] mx-2 bg-gray-100 shadow-lg mt-5 rounded overflow-auto min-h-[600px]">
   <h1 class="text-xl font-bold mb-5 bg-indigo-500 text-white p-5">{title}</h1>
   <div class="p-2">
     <p>{content}</p>
