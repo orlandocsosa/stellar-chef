3d305af25fdfc0e7ca23b1c386db23a7f998f270
Joaquin Akerman <55959401+JoaquinAkerman@users.noreply.github.com>
2024-02-15 14:20:47 -0300

    Add claimantsStore subscription in buildCreateClamableBalanceOperation

--- src/services/stellar/operations/createClamableBalanceOperation.ts
+++ src/services/stellar/operations/createClamableBalanceOperation.ts
@@ -1,3 +1,10 @@
+import { claimantsStore } from '../../../utils/stores/claimantsStore';
+
 export function buildCreateClamableBalanceOperation(formData: FormData): any {
   console.log('formData', formData);
+  const unsubscribe = claimantsStore.subscribe((claimants) => {
+    console.log('claimantsStore from buildCreateClamableBalanceOperation', claimants);
+  });
+
+  unsubscribe();
 }
