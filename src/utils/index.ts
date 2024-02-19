18c47a0b4217794a867b49eeda09be78486326ed
eloptimistadelcode <153465933+eloptimistadelcode@users.noreply.github.com>
2024-02-19 17:57:19 -0300

    Add parseEntriesValues utils function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,4 +2,16 @@ function allowOnlyAlphanumeric(inputValue: string): string {
   return inputValue.replace(/[^a-zA-Z0-9]/g, '');
 }
 
-export { allowOnlyAlphanumeric };
+function parseEntriesValues<T>(data: FormData | URLSearchParams): T {
+  const newObj: Record<string, string> = {};
+
+  for (const [key, value] of data.entries()) {
+    if (typeof value === 'string') {
+      newObj[key] = value;
+    }
+  }
+
+  return newObj as T;
+}
+
+export { allowOnlyAlphanumeric, parseEntriesValues };
