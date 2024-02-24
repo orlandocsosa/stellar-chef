5c5d6c5c2285ab18f0915d1e4554d6cfde41df25
eloptimistadelcode <153465933+eloptimistadelcode@users.noreply.github.com>
2024-02-24 15:55:56 -0300

    Add ClaimableBalancesRecords component

---
+++ src/components/claimable-balance/ClaimableBalancesRecords.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import type { Horizon } from 'stellar-sdk';
+  import Card from '../salient/Card.svelte';
+
+  export let claimableBalances: Horizon.ServerApi.ClaimableBalanceRecord[];
+</script>
+
+<Card className="w-[650px] font-light">
+  <div class="flex flex-col gap-10">
+    {#each claimableBalances as { id, amount, asset, claimants }}
+      <div class="flex flex-col gap-2 bg-gray-50 border border-gray-200 p-5 rounded-md">
+        <p class="break-words">ID: <span class="text-gray-600">{id}</span></p>
+        <p>Amount: <span class="text-gray-600">{amount}</span></p>
+        <p class="break-words">Asset: <span class="text-gray-600">{asset}</span></p>
+
+        {#each claimants as { destination, predicate }, i}
+          <div class="flex flex-col gap-2">
+            <p class="text-lg">
+              Claimant #{i + 1}: <span class="text-gray-600 text-base break-words">{destination}</span>
+            </p>
+
+            <p>Predicate:</p>
+            <pre class="text-gray-600 break-words">{JSON.stringify(predicate, null, 2)}</pre>
+          </div>
+        {/each}
+      </div>
+    {/each}
+  </div>
+</Card>
