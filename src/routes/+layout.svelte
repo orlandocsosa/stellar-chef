9edc1f8634dc9cec554ef11c790ac1f20c10d2af
eloptimistadelcode <153465933+eloptimistadelcode@users.noreply.github.com>
2024-02-29 19:20:15 -0300

    Feature/Home page (#31)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,4 +5,9 @@
 <head>
   <title>Stellar Chef</title>
 </head>
+
+<header class="w-full flex flex-row items-center justify-center gap-16 bg-blue-600 h-20 mb-10">
+  <a class="text-white" href="/stellar-chef/">Recipes</a>
+  <a class="text-white" href="/stellar-chef/faucet">Faucet</a>
+</header>
 <slot />
