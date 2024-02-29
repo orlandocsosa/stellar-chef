c5ef08c53a2a82802a605a5c92b68591c1016bf7
eloptimistadelcode <153465933+eloptimistadelcode@users.noreply.github.com>
2024-02-29 19:16:49 -0300

    Add RecipeCard component

---
+++ src/components/salient/RecipeCard.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  export let title = '';
+  export let link = '';
+  export let description = '';
+</script>
+
+<li class="bg-gray-50 border border-gray-200 rounded-md p-3">
+  <h3 class="text-xl"><a href={link}>{title}</a></h3>
+  <p class="text-gray-600">{description}</p>
+</li>
