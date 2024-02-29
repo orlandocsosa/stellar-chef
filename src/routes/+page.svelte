5895a27f1fcd5f5fed9a5a6e75df01e1f1446518
eloptimistadelcode <153465933+eloptimistadelcode@users.noreply.github.com>
2024-02-29 19:17:17 -0300

    Add recipes list in home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,50 @@
+<script>
+  import RecipeCard from '../components/salient/RecipeCard.svelte';
+
+  const recipes = [
+    {
+      title: 'Asset issuance',
+      link: 'asset-issuance',
+      description: 'Asset creation with distributor and holders'
+    },
+    {
+      title: 'Offers',
+      link: 'offers',
+      description: 'Create and manage offers'
+    },
+    {
+      title: 'Claimable balance',
+      link: 'claimable-balance',
+      description: 'Claim, search, and, create claimable balances'
+    },
+    {
+      title: 'Clawback',
+      link: 'clawback',
+      description: 'Asset clawback'
+    },
+    {
+      title: 'Create accounts',
+      link: 'create-account',
+      description: 'Create accounts'
+    },
+    {
+      title: 'Freeze assets',
+      link: 'freeze-assets',
+      description: 'Freeze assets'
+    }
+  ];
+</script>
+
 <svelte:head>
   <title>Stellar Chef</title>
 </svelte:head>
 
-<h1>Home</h1>
+<div class="flex flex-col w-[500px] m-auto gap-5 justify-center">
+  <h2 class="text-2xl">Recipes</h2>
+
+  <ul class="flex flex-col gap-5">
+    {#each recipes as { description, link, title }}
+      <RecipeCard {title} link={`/stellar-chef/recipe/${link}`} {description} />
+    {/each}
+  </ul>
+</div>
