55145161b5847a2fb15b687d10bac4a7a5572fcd
eloptimistadelcode <facu01381@gmail.com>
2024-03-24 19:06:22 -0300

    Add Calculators link in header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,6 +10,7 @@
 <header class="w-full flex flex-row items-center justify-center gap-16 bg-blue-600 h-20 mb-10">
   <a class="text-white" href="/stellar-chef/">Recipes</a>
   <a class="text-white" href="/stellar-chef/faucet">Faucet</a>
+  <a class="text-white" href="/stellar-chef/calculator">Calculators</a>
 </header>
 <SvelteToast />
 <slot />
