dd8facf8847d31aa8362fd09d7784b184d849a5a
eloptimistadelcode <facu01381@gmail.com>
2024-03-24 15:51:16 -0300

    Add border for the salient Card component

--- src/components/salient/Card.svelte
+++ src/components/salient/Card.svelte
@@ -2,6 +2,6 @@
   export let className = '';
 </script>
 
-<div class="{className && `${className} `}rounded-2xl bg-white p-6 shadow-xl shadow-slate-900/10">
+<div class="{className && `${className} `}rounded-2xl bg-white p-6 shadow-xl border shadow-slate-900/10">
   <slot />
 </div>
