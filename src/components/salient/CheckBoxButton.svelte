7a90fc40445a04934ad123769efb6ba5dedbaf3e
eloptimistadelcode <facu01381@gmail.com>
2024-04-01 15:37:37 -0300

    Create CheckBoxButton.svelte

---
+++ src/components/salient/CheckBoxButton.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  export let checked = false;
+  export let text: string;
+</script>
+
+<label>
+  <p
+    class="{checked ? 'text-blue-600 border-blue-600' : 'text-gray-600 border-gray-200'} 
+        border-2 rounded-full py-1 px-2 cursor-pointer"
+  >
+    {text}
+  </p>
+  <input class="hidden" type="checkbox" bind:checked />
+</label>
